7201fc67496b795878cfdf6b4d3e21abbd0bd09f
mjarraya <mjarraya@student.42.fr>
2019-06-27 17:29:10 +0200

    mongoose intro & promise

---
+++ mongoose-intro/index.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
+mongoose.connect("mongodb://localhost/dbName141");
+
+const userSchema = new Schema({
+  username: String
+});
+
+const User = new mongoose.model("User", userSchema);
+
+User.create({
+  username: "cocoapuff"
+});
+
+/*
+for (let i = 1; i < 5; i++) {
+  User.create({
+    username: `cocoapuff${i}`
+  });
+}
+*/
+/*
+User.insertMany([
+  {
+    username: "jim"
+  },
+  {
+    username: "dwight"
+  },
+  {
+    username: "pam"
+  }
+]);
+*/
+
+const userDwight = User.find({ username: "blabla" })
+  .then(data => {
+    //   fulfilled
+    console.log("data: ", data);
+  })
+  .catch(err => {
+    //   rejected
+    console.log("error: ", err);
+  });
+
+console.log(userDwight); // pending
