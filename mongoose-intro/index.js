9fd61cb5f6596c2cc3012306fdb436dc8c610570
mjarraya <mjarraya@student.42.fr>
2019-06-28 10:38:55 +0200

    mongoose create read update delete

--- mongoose-intro/index.js
+++ mongoose-intro/index.js
@@ -1,53 +1,16 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const express = require("express");
+const app = express();
 
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useFindAndModify", false);
-mongoose.set("useCreateIndex", true);
+const User = require("./models/User");
 
-mongoose.connect("mongodb://localhost/dbName141");
-
-const userSchema = new Schema({
-  username: String
+app.get("/", (req, res) => {
+  User.find({})
+    .then(users => {
+      res.send(users);
+    })
+    .catch(err => {
+      res.send(err);
+    });
 });
 
-const User = new mongoose.model("User", userSchema);
-
-// creates a user in the 'users' collection
-User.create({
-  username: "cocoapuff"
-});
-
-/*
-for (let i = 1; i < 5; i++) {
-  User.create({
-    username: `cocoapuff${i}`
-  });
-}
-*/
-/*
-User.insertMany([
-  {
-    username: "jim"
-  },
-  {
-    username: "dwight"
-  },
-  {
-    username: "pam"
-  }
-]);
-*/
-
-// finds the users in the 'users' collection
-const userDwight = User.find({ username: "blabla" })
-  .then(data => {
-    //   fulfilled
-    console.log("data: ", data);
-  })
-  .catch(err => {
-    //   rejected
-    console.log("error: ", err);
-  });
-
-console.log(userDwight); // pending
+app.listen(3000);
--- mongoose-intro/models/User.js
+++ mongoose-intro/models/User.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
 
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
+mongoose.connect("mongodb://localhost/dbName141");
+
 // const Schema = mongoose.Schema;
 const { Schema } = mongoose;
 
@@ -12,6 +18,7 @@ const userSchema = new Schema({
   username: {
     type: String,
     required: true,
+    unique: true,
     lowercase: true,
     trim: true
   },
@@ -43,6 +50,7 @@ const userSchema = new Schema({
     }
   },
   role: {
+    type: String,
     enum: ["user", "mod", "admin"],
     default: "user"
   },
@@ -55,3 +63,93 @@ const userSchema = new Schema({
 });
 
 const User = mongoose.model("User", userSchema);
+
+module.exports = User;
+
+/*
+User.create({
+  username: "paul",
+  email: "paul-lenhard@ironhack.com",
+  password: "12345678",
+  age: 27,
+  role: "user",
+  website: "https://paul.com",
+  phoneNumbers: [123456, 7891011]
+})
+  .then(data => {
+    console.log("User successfully created: ", data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+
+// User.findById("5d15c7db3d774909e915afdb")
+// User.findOne({ username: "paul" })
+// User.find({ password: '12345678' })
+
+User.findOne({ username: "paul" })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+// User.updateOne({ username: 'paul' }, { birthdate: '10-23-1995', password: '12345678' })
+
+User.updateMany({ password: "qwertyuiop" }, { password: "12345678" })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+
+User.findByIdAndUpdate("5d15c830d8ef120a13dc9563", { role: "user" });
+User.findOneAndUpdate({ username: "paul" }, { role: "user" }, { new: true })
+  .then(user => {
+    console.log(user);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+User.deleteOne({ username: "paul" })
+  .then(user => {
+    console.log(user);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+User.findOneAndDelete({ username: "devan" })
+  .then(user => {
+    console.log(user);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+User.deleteMany({ isActive: false })
+  .then(data => {
+    console.log(data); // no documents returned
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+//   Delete a collection
+User.collection.drop();
+
+// Get a count
+User.countDocuments({ isActive: true })
+  .then(count => {
+    console.log(count); // number of active users
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+  */
